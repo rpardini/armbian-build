325ff5229db9aa9a5a3cfac26b382984c8e2d2fb
Ricardo Pardini <ricardo@pardini.net>
2022-05-06 10:31:24 +0200

    [NOPR] rpardini's custom extensions (mostly cloud-images) [NO PR]
    - `java-build-machine` extension; includes GraalVM native-image for arm64
      - add zlib1g-dev for graal native builds.
    - java-build-machine: bump to GraalVM 22.2.0

---
+++ userpatches/extensions/build-machine-image.sh
@@ -0,0 +1,32 @@
+function extension_prepare_config__build_machine_image() {
+	display_alert "Target image will be a build machine" "${EXTENSION}" "info"
+	#export BUILD_KSRC=yes
+}
+
+function host_dependencies_known__add_host_deps_to_package_list_board() {
+	display_alert "Adding build dependencies for" "${EXTENSION}" "info"
+	# FINAL_HOST_DEPS is exported by the host_dependencies_ready() hook.
+	display_alert "Adding to package list, for build machine" "${FINAL_HOST_DEPS}" "debug"
+
+	# basic deps needed for building, from basic-deps.sh
+	local basic_deps="uuid-runtime dialog psmisc acl curl gnupg gawk"
+
+	# Add deps to PACKAGE_LIST, so it's cached in rootfs.
+	export PACKAGE_LIST="${PACKAGE_LIST} ${FINAL_HOST_DEPS} ${basic_deps}"
+	display_alert "Added to package list, for build machine" "${FINAL_HOST_DEPS} ${basic_deps}" "debug"
+}
+
+function post_install_kernel_debs__copy_kernel_sources_and_headers_to_rootfs() {
+	display_alert "Adding source to BM's /root" "${EXTENSION}" "info"
+	declare source_deb="${CHOSEN_KSRC}_${REVISION}_all.deb"
+	if [[ -f "${DEB_STORAGE}/${source_deb}" ]]; then
+		run_host_command_logged ls -lah "${DEB_STORAGE}/${source_deb}"
+		run_host_command_logged cp -vp "${DEB_STORAGE}/${source_deb}" "${SDCARD}"/usr/src
+	else
+		display_alert "Can't find kernel source deb, wont be included in BM image" "${source_deb}" "warn"
+	fi
+}
+
+function pre_umount_final_image__add_build_machine_suffix_to_version() {
+	export version="${version}-armbian-bm"
+}
