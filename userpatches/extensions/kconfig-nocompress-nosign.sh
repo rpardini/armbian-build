62d827e0f65559e5890506af582078ab512b6c52
Ricardo Pardini <ricardo@pardini.net>
2022-09-25 18:25:05 +0200

    [NOPR] rpardini: exts: custom extensions (mostly cloud-images) [NO PR]
    - `java-build-machine` extension; includes GraalVM native-image for arm64
      - add zlib1g-dev for graal native builds.
    - java-build-machine: bump to GraalVM 22.2.0
    - rpardini exts don't fail if no resolved.conf (kinetic etc, probably underlying problem)
    - rpardini ext cloudinit: force systemd-resolved install on kinetic
    - rpardini: add extension `docker`, to pre-install `docker-ce` and such after adding gpg and apt
    - rpardini: cloud: install wpasupplicant, always; cloud images can run on wifi-only SBCs too
    - rpardini: hack/fix the fastboot extension to (barely) work again
    - rpardini: cloud-init: introduce last-minute hook for network-config `cloud_init_modify_network_config()`
    - rpardini: usbgadget, make more useful, work with modules; autoconfig NM for usb0-dhcp
    - rpardini: exts: `cloud-metadata` ext, for real clouds, not SBC-fake-cloud.
    - rpardini: ext: usbgadget fixes, shortcircuit build breaker fix
    - aggregation.py -> rpardini exts
    - rpardini exts uefi-rescue fix for CLOUD_INIT_INSTANCE_ID
    - rpardini: exts: add extension for (previously-EXTRAWIFI) `rtl88x2bu` from https://github.com/cilynx/rtl88x2bu (ARCHIVE: extrawifi now works again)
    - rpardini: exts: kconfig-nocompress-nosign.sh
    - rpardini: exts: curb cloud-init debian logging
    - rpardini: ext: kconfig-nocompress-nosign.sh: curb logging
    - rpardini: exts: @TODO for e2img
    - rpardini: ext: bm no longer has ksrc

---
+++ userpatches/extensions/kconfig-nocompress-nosign.sh
@@ -0,0 +1,42 @@
+# @TODO: internalize this to core?
+# @TODO: use similar to set LOCALVERSION, might be more efficient than passing via cmdline
+
+function armbian_kernel_config__disable_module_compression() {
+	display_alert "Disabling module compression" "MODULE_COMPRESS" "info"
+	kernel_config_set_n CONFIG_MODULE_COMPRESS_XZ
+	kernel_config_set_n CONFIG_MODULE_COMPRESS_ZSTD
+	kernel_config_set_n CONFIG_MODULE_COMPRESS_GZIP
+	kernel_config_set_y CONFIG_MODULE_COMPRESS_NONE
+}
+
+function armbian_kernel_config__disable_module_signing() {
+	display_alert "Disabling module signing" "CONFIG_MODULE_SIG" "info"
+	kernel_config_set_n CONFIG_SECURITY_LOCKDOWN_LSM
+	kernel_config_set_n CONFIG_MODULE_SIG
+}
+
+function DISABLED_armbian_kernel_config__enable_module_compression() {
+	display_alert "Enabling module compression" "MODULE_COMPRESS" "info"
+	kernel_config_set_n CONFIG_MODULE_COMPRESS_XZ
+	kernel_config_set_y CONFIG_MODULE_COMPRESS_ZSTD
+	kernel_config_set_n CONFIG_MODULE_COMPRESS_GZIP
+	kernel_config_set_n CONFIG_MODULE_COMPRESS_NONE
+}
+
+function kernel_config_set_m() {
+	declare module="$1"
+	display_alert "Enabling kernel module" "${module}=m" "debug"
+	run_host_command_logged ./scripts/config --module "$module"
+}
+
+function kernel_config_set_y() {
+	declare config="$1"
+	display_alert "Enabling kernel config/built-in" "${config}=y" "debug"
+	run_host_command_logged ./scripts/config --enable "${config}"
+}
+
+function kernel_config_set_n() {
+	declare config="$1"
+	display_alert "Disabling kernel config/module" "${config}=n" "debug"
+	run_host_command_logged ./scripts/config --disable "${config}"
+}
