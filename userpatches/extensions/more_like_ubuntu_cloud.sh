699ff4b574f400929c13323ac611dec909f0a3f9
Ricardo Pardini <ricardo@pardini.net>
2022-09-25 18:25:05 +0200

    [NOPR] rpardini's custom extensions (mostly cloud-images) [NO PR]
    - `java-build-machine` extension; includes GraalVM native-image for arm64
      - add zlib1g-dev for graal native builds.
    - java-build-machine: bump to GraalVM 22.2.0
    - rpardini exts don't fail if no resolved.conf (kinetic etc, probably underlying problem)
    - rpardini ext cloudinit: force systemd-resolved install on kinetic
    - rpardini: add extension `docker`, to pre-install `docker-ce` and such after adding gpg and apt
    - rpardini: cloud: install wpasupplicant, always; cloud images can run on wifi-only SBCs too
    - rpardini: hack/fix the fastboot extension to (barely) work again
    - rpardini: cloud-init: introduce last-minute hook for network-config `cloud_init_modify_network_config()`
    - rpardini: usbgadget, make more useful, work with modules; autoconfig NM for usb0-dhcp
    - rpardini: exts: `cloud-metadata` ext, for real clouds, not SBC-fake-cloud.
    - rpardini: ext: usbgadget fixes, shortcircuit build breaker fix
    - aggregation.py -> rpardini exts
    - rpardini exts uefi-rescue fix for CLOUD_INIT_INSTANCE_ID
    - rpardini: exts: add extension for (previously-EXTRAWIFI) `rtl88x2bu` from https://github.com/cilynx/rtl88x2bu (ARCHIVE: extrawifi now works again)
    - rpardini: exts: kconfig-nocompress-nosign.sh

---
+++ userpatches/extensions/more_like_ubuntu_cloud.sh
@@ -0,0 +1,82 @@
+# Remove packages that dont belong in a cloud image.
+# Wifi, Chrony, etc.
+function extension_prepare_config__prepare_mluc() {
+	display_alert "Removing uncloudlike packages: vnstat, chrony, etc." "${EXTENSION}" "info"
+	# crda is needed for linux-modules-extra which is essential albeit the name
+	# crda in turn depends on iw, wireless-regdb
+	remove_packages vnstat chrony unattended-upgrades wpasupplicant rng-tools networkd-dispatcher hping3 selinux-policy-default dkms vnstat
+	#remove_packages armbian-config # contains the armbian neofetch. keep it
+	# remove most packages from additional: find . -name packages.additional | xargs cat  | sort | uniq | xargs echo
+	remove_packages alsa-utils aptitude avahi-autoipd btrfs-progs cracklib-runtime evtest f2fs-tools f3 haveged \
+		hdparm i2c-tools iotop iozone3 iperf3 iputils-arping libcrack2 libdigest-sha-perl \
+		libproc-processtable-perl mc nfs-common ntfs-3g rfkill \
+		wireless-tools
+
+	# removing all those packages makes the size calculation go too low. compensate.
+	if [[ ${EXTRA_ROOTFS_MIB_SIZE} -le 256 ]]; then
+		export EXTRA_ROOTFS_MIB_SIZE=256
+		display_alert "Setting EXTRA_ROOTFS_MIB_SIZE: ${EXTRA_ROOTFS_MIB_SIZE}" "${EXTENSION}" "info"
+	fi
+
+	add_packages_to_rootfs bash-completion ssh-import-id curl dnsutils dosfstools ethtool git jq lsof nano pciutils pv screen unzip wget zsh tmux
+	add_packages_to_image systemd-timesyncd # chrony does not play well with systemd / qemu-agent.
+
+	export EXTRA_BSP_NAME="${EXTRA_BSP_NAME}-mluc" # Unique bsp name for this extension: more like ubuntu cloud
+}
+
+# Tweak the BSP, removing a bunch of stuff that's great for interactive end-users and memory-deprived systems,
+# but not so much for something is is provisioned like a cloud instance.
+# @TODO: terrible way, just delete stuff, most stuff will error out at runtime. rethink.
+post_family_tweaks_bsp__be_more_like_ubuntu_cloud() {
+	#display_alert "Starting files at BSP at ${destination}" "more-like-ubuntu-cloud" "info"
+	#find "$destination" -type f
+
+	display_alert "Removing stuff from BSP" "more-like-ubuntu-cloud" "info"
+	# Lets obliterate stuff in mass.
+	find "$destination" -type f | grep \
+		-e "polkit" \
+		-e "bootsplash" \
+		-e "autologin" \
+		-e "firstrun" \
+		-e "journald" \
+		-e "armbian-config" \
+		-e "resize" \
+		-e "zram" \
+		-e "periodic" \
+		-e "interfaces\.default" \
+		-e "NetworkManager" \
+		-e "profile-sync-daemon" \
+		-e "logrotate" \
+		-e "ramlog" | xargs rm
+
+	display_alert "Hacking at the BSP" "more-like-ubuntu-cloud" "info"
+	# remove a bunch of stuff from bsp so it behaves more like regular ubuntu
+	RM_OPTIONS="-f"
+	rm ${RM_OPTIONS} "$destination"/etc/apt/apt.conf.d/02-armbian-compress-indexes
+
+	rm ${RM_OPTIONS} "$destination"/etc/cron.d/armbian-truncate-logs
+	rm ${RM_OPTIONS} "$destination"/etc/cron.d/armbian-updates
+	rm ${RM_OPTIONS} "$destination"/etc/cron.daily/armbian-ram-logging
+
+	rm ${RM_OPTIONS} "$destination"/etc/default/armbian-ramlog.dpkg-dist
+	rm ${RM_OPTIONS} "$destination"/etc/default/armbian-zram-config.dpkg-dist
+
+	rm ${RM_OPTIONS} "$destination"/etc/profile.d/armbian-check-first-login.sh
+
+	rm ${RM_OPTIONS} "$destination"/etc/lib/systemd/system/systemd-journald.service.d/override.conf
+
+	rm ${RM_OPTIONS} "$destination"/etc/lib/systemd/system/armbian-firstrun.service
+	rm ${RM_OPTIONS} "$destination"/etc/lib/systemd/system/armbian-ramlog.service
+	rm ${RM_OPTIONS} "$destination"/etc/lib/systemd/system/armbian-resize-filesystem.service
+	rm ${RM_OPTIONS} "$destination"/etc/lib/systemd/system/armbian-zram-config.service
+
+	rm ${RM_OPTIONS} "$destination"/lib/systemd/system/armbian-firstrun-config.service
+	rm ${RM_OPTIONS} "$destination"/lib/systemd/system/armbian-firstrun.service
+	rm ${RM_OPTIONS} "$destination"/lib/systemd/system/armbian-resize-filesystem.service
+	rm ${RM_OPTIONS} "$destination"/lib/systemd/system/armbian-zram-config.service
+	rm ${RM_OPTIONS} "$destination"/lib/systemd/system/armbian-disable-autologin.service
+	rm ${RM_OPTIONS} "$destination"/lib/systemd/system/armbian-ramlog.service
+
+	#display_alert "Remaining files at BSP at ${destination}" "more-like-ubuntu-cloud" "info"
+	#find "$destination" -type f
+}
