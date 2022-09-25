b0c8a0f294d5244e7797a642bb8c71f179f31f2e
Ricardo Pardini <ricardo@pardini.net>
2022-09-25 18:25:05 +0200

    [NOPR] rpardini's custom extensions (mostly cloud-images) [NO PR]
    - `java-build-machine` extension; includes GraalVM native-image for arm64
      - add zlib1g-dev for graal native builds.
    - java-build-machine: bump to GraalVM 22.2.0
    - rpardini exts don't fail if no resolved.conf (kinetic etc, probably underlying problem)
    - rpardini ext cloudinit: force systemd-resolved install on kinetic
    - rpardini: add extension `docker`, to pre-install `docker-ce` and such after adding gpg and apt
    - rpardini: cloud: install wpasupplicant, always; cloud images can run on wifi-only SBCs too
    - rpardini: hack/fix the fastboot extension to (barely) work again
    - rpardini: cloud-init: introduce last-minute hook for network-config `cloud_init_modify_network_config()`
    - rpardini: usbgadget, make more useful, work with modules; autoconfig NM for usb0-dhcp
    - rpardini: exts: `cloud-metadata` ext, for real clouds, not SBC-fake-cloud.
    - rpardini: ext: usbgadget fixes, shortcircuit build breaker fix
    - aggregation.py -> rpardini exts
    - rpardini exts uefi-rescue fix for CLOUD_INIT_INSTANCE_ID

---
+++ userpatches/extensions/uefi-rescue.sh
@@ -0,0 +1,17 @@
+enable_extension "rootfs_e2img_inside_rootfs" # rootfs in rootfs, will be exported, not really included.
+
+user_config__700_rescue_uefi_arm64_config() {
+	export CLOUD_INIT_EXTRA_VERSION="rescue"
+	export UEFI_GRUB_TERMINAL="serial console"            # this will go in grub.d config, so Grub displays on serial and console
+	export CLOUD_INIT_CONFIG_LOCATION="/boot"             # use /boot for cloud-init as well
+	unset CLOUD_INIT_USER_DATA_URL                        # for rescue image, don't use the URL, instead cloud-init will default to 'files'
+	unset CLOUD_INIT_INSTANCE_ID                          # for rescue image, don't specify the instance-id; cloud-init will default to armbian-BOARD
+	export UEFI_GRUB_DISTRO_NAME="ArmbianRescue"          # To signal this is the rescue rootfs/grub
+	export ROOTFS_IN_ROOTFS_EXPORT_ONLY="yes"             # If yes, e2img will not be included, but only copied to output/images/xx.e2img
+	export DONT_EXPORT_REGULAR_IMG="yes"                  # No use for a non-e2img at the output, it won't ever be used.
+	export EXTRA_ROOTFS_MIB_SIZE=256                      # arm64 image requires a bit more space for Grub?
+	export SERIALCON="ttyAMA0"                            # These are mostly used in cloud environments, where a console exists.
+	[[ "${BOARD}" == *x86* ]] && export SERIALCON="ttyS0" # For Oracle VMs on AMD micro stuff
+	export UEFI_ENABLE_BIOS_AMD64="no"                    # Disable the BIOS-too aspect of UEFI on amd64, this is just uefi
+	display_alert "Rescue variant" "enabled for ${BOARD} with console at ${SERIALCON}" "info"
+}
