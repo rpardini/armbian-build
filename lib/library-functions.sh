609526b664977081b7afd1fbc611ca124135ceda
Ricardo Pardini <ricardo@pardini.net>
2022-12-19 19:00:40 +0100

    armbian-next: introduce `ORAS` tooling; pull and push functions & downloader/launcher

--- lib/library-functions.sh
+++ lib/library-functions.sh
@@ -411,6 +411,15 @@ source "${SRC}"/lib/functions/general/git.sh
 #set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
 set -o errtrace # trace ERR through - enabled
 set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
+### lib/functions/general/oci-oras.sh
+# shellcheck source=lib/functions/general/oci-oras.sh
+source "${SRC}"/lib/functions/general/oci-oras.sh
+
+# no errors tolerated. invoked before each sourced file to make sure.
+#set -o pipefail  # trace ERR through pipes - will be enabled "soon"
+#set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
+set -o errtrace # trace ERR through - enabled
+set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
 ### lib/functions/general/python-tools.sh
 # shellcheck source=lib/functions/general/python-tools.sh
 source "${SRC}"/lib/functions/general/python-tools.sh
