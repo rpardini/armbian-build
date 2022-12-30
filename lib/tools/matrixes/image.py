213cd8f382e94ab8fb89e3ac332e63d3a2033deb
Ricardo Pardini <ricardo@pardini.net>
2022-12-30 12:20:53 +0100

    WiP: GHA Workflow generator delusion pt2
      - WiP: GHA Workflow generator delusion: experiment with aggregating kernel by git source+branch to group version resolution
      - WiP: GHA Workflow generator delusion: back to simple KernelPrep + Kernels + Images, no rootfs nor uboot
      - WiP: GHA Workflow generator delusion: fixes; fix dependency
      - WiP: GHA Workflow generator delusion: introduce `BaseAggregator::show_entries()` abstract
      - WiP: GHA Workflow generator delusion: half-abstract away the GHA Jobs, outputs, inputs, conditions, steps, needs; format
      - WiP: GHA Workflow generator delusion: split off Image from Input
    - WiP: GHA Workflow generator delusion pt1.5
      - WiP: GHA Workflow generator delusion: split
      - WiP: GHA Workflow generator delusion: aggregate rootfs (half-done)
      - WiP: GHA Workflow generator delusion: coin flip for kernels up-to-date, outputs for skipped, use `always()`
      - WiP: GHA Workflow generator delusion: coin flip for kernels up-to-date
      - WiP: GHA Workflow generator delusion: prepare job for all kernels
      - WiP: GHA Workflow generator delusion: aggregator pt 2/3
      - WiP: GHA Workflow generator delusion: aggregator pt 1/3
      - WiP: GHA Workflow generator delusion: jobs with needs
      - WiP: GHA Workflow generator delusion: +split, add rootfs cli, add refs
      - WiP: GHA Workflow generator delusion: split

---
+++ lib/tools/matrixes/image.py
@@ -0,0 +1,81 @@
+import logging
+
+from matrixes.base import BaseAggregator, BaseMatrixAggregate
+from matrixes.gha import BaseWorkflowJob, WorkflowFactory
+from matrixes.input import MatrixInput
+from matrixes.kernel import MatrixKernel
+from matrixes.rootfs import MatrixRootFileSystemCLI
+from matrixes.uboot import MatrixUboot
+
+log: logging.Logger = logging.getLogger("matrix_kernel")
+
+
+class ImageAggregator(BaseAggregator):
+	"""A ridiculous non-aggregator; every input is an output"""
+
+	def __init__(self, inputs: list[MatrixInput]):
+		super().__init__(inputs)
+		self.images: list[MatrixImage] = [MatrixImage(entry.image_id(), entry, [entry], self) for entry in inputs]
+
+	def produce_gha_jobs(self, wf: WorkflowFactory):
+		for image in self.images:
+			wf.add_job(ImageBuildJob(self, image))
+
+	def show_entries(self):
+		log.info(f"Parsed {len(self.images)} images")
+		for image in self.images:
+			log.info(f"{image}")
+
+
+class MatrixImage(BaseMatrixAggregate):
+
+	def __init__(self, aggregate_id: str, item: MatrixInput, all_items: list[MatrixInput], aggregator: "ImageAggregator"):
+		super().__init__(aggregate_id, item, all_items)
+
+		self.ref_kernel: MatrixKernel = item.ref_kernel
+		self.ref_u_boot: MatrixUboot = item.ref_u_boot
+		self.ref_root_fs_cli: MatrixRootFileSystemCLI = item.ref_root_fs_cli
+
+		self.board_id: str = self.sanity_check_same(lambda x: x.board_id)
+		self.aggregator: ImageAggregator = aggregator
+		self.branch: str = self.sanity_check_same(lambda i: i.BRANCH)
+		self.release: str = self.sanity_check_same(lambda i: i.RELEASE)
+		self.arch: str = self.sanity_check_same(lambda i: i.ARCH)
+		self.boards: set[str] = self.unique(lambda i: i.board_id)
+		if aggregate_id is not None:
+			for one_item in all_items:
+				one_item.ref_image = self
+
+	def __str__(self) -> str:
+		return f'<Image id="{self.aggregate_id}" branch="{self.branch}" />'
+
+	def gha_job_definition(self):
+		gha_job = {"runs-on": ["self-hosted", "Linux", "armbian"]}
+		expression = f"needs.{self.ref_kernel.gha_job_id()}.outputs.up-to-date"
+		gha_job["if"] = '${{ always() && ' + expression + " == 'no' }}"
+		steps = []
+		fake_step = {"name": f"Image CLI '{self.board_id}-{self.branch}'", "run": f'echo "fake image: {self.board_id}-{self.branch}"'}
+		steps.append(fake_step)
+		gha_job["steps"] = steps
+		gha_job["needs"] = []
+		if self.ref_kernel:
+			gha_job["needs"].append(self.ref_kernel.gha_job_id())
+		if self.ref_root_fs_cli:
+			gha_job["needs"].append(self.ref_root_fs_cli.gha_job_id())
+		return gha_job
+
+
+class ImageBuildJob(BaseWorkflowJob):
+	def __init__(self, i_aggr: ImageAggregator, image: MatrixImage):
+		super().__init__(
+			f"image_cli_{image.board_id}-{image.branch}-{image.release}",
+			f"Image build for {image.board_id}-{image.branch}-{image.release}")
+		self.i_aggr: ImageAggregator = i_aggr
+		self.image: MatrixImage = image
+
+		build_step = self.add_step(f"build_image_{image.aggregate_id}", f"Build Image {image.aggregate_id}")
+		build_step.run = f'echo "fake image: {image.aggregate_id}"'
+
+		if self.image.ref_kernel:
+			uptodate_input = self.add_job_input_from_needed_job_output(self.image.ref_kernel.kjo_uptodate)
+			self.add_condition_from_input(uptodate_input, "== 'no'")
