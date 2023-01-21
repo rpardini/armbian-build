5102790aeadf0598cced37fc8ba9a1a542cc64fc
Ricardo Pardini <ricardo@pardini.net>
2023-01-21 05:29:25 +0100

    rpardini: ext: kconfig-nocompress-nosign.sh: curb logging

--- userpatches/extensions/kconfig-nocompress-nosign.sh
+++ userpatches/extensions/kconfig-nocompress-nosign.sh
@@ -2,28 +2,25 @@
 # @TODO: use similar to set LOCALVERSION, might be more efficient than passing via cmdline
 
 function armbian_kernel_config__disable_module_compression() {
-	display_alert "Disabling module compression" "MODULE_COMPRESS" "warn"
+	display_alert "Disabling module compression" "MODULE_COMPRESS" "info"
 	kernel_config_set_n CONFIG_MODULE_COMPRESS_XZ
 	kernel_config_set_n CONFIG_MODULE_COMPRESS_ZSTD
 	kernel_config_set_n CONFIG_MODULE_COMPRESS_GZIP
 	kernel_config_set_y CONFIG_MODULE_COMPRESS_NONE
-	display_alert "Disabling module compression" "DONE MODULE_COMPRESS" "warn"
 }
 
 function armbian_kernel_config__disable_module_signing() {
-	display_alert "Disabling module signing" "CONFIG_MODULE_SIG" "warn"
+	display_alert "Disabling module signing" "CONFIG_MODULE_SIG" "info"
 	kernel_config_set_n CONFIG_SECURITY_LOCKDOWN_LSM
 	kernel_config_set_n CONFIG_MODULE_SIG
-	display_alert "Disabling module signing" "DONE CONFIG_MODULE_SIG" "warn"
 }
 
 function DISABLED_armbian_kernel_config__enable_module_compression() {
-	display_alert "Enabling module compression" "MODULE_COMPRESS" "warn"
+	display_alert "Enabling module compression" "MODULE_COMPRESS" "info"
 	kernel_config_set_n CONFIG_MODULE_COMPRESS_XZ
 	kernel_config_set_y CONFIG_MODULE_COMPRESS_ZSTD
 	kernel_config_set_n CONFIG_MODULE_COMPRESS_GZIP
 	kernel_config_set_n CONFIG_MODULE_COMPRESS_NONE
-	display_alert "Enabling module compression" "DONE MODULE_COMPRESS" "warn"
 }
 
 function kernel_config_set_m() {
