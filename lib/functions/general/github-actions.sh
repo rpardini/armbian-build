b7afbf63f7547d67acc24e40a5b806a1146a8ae4
Ricardo Pardini <ricardo@pardini.net>
2023-02-03 15:36:28 +0100

    artifacts -> OCI --> try 1

--- lib/functions/general/github-actions.sh
+++ lib/functions/general/github-actions.sh
@@ -1,7 +1,7 @@
 function github_actions_add_output() {
 	# if CI is not GitHub Actions, do nothing
 	if [[ "${CI}" != "true" ]] && [[ "${GITHUB_ACTIONS}" != "true" ]]; then
-		display_alert "Not running in GitHub Actions, not adding output" "'${*}'" "debug"
+		display_alert "Not running in GitHub Actions, not adding output" "'${*}'" "warn"
 		return 0
 	fi
 
@@ -14,5 +14,5 @@ function github_actions_add_output() {
 	local output_value="$*"
 
 	echo "${output_name}=${output_value}" >> "${GITHUB_OUTPUT}"
-	display_alert "Added GHA output" "'${output_name}'='${output_value}'" "info"
+	display_alert "Added GHA output" "'${output_name}'='${output_value}'" "warn"
 }
