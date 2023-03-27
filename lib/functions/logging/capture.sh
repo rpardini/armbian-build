cf692a8620801dfccdc88284bed52fdcd854de5a
Ricardo Pardini <ricardo@pardini.net>
2023-03-27 13:38:10 +0200

    capture: set globals `CAPTURED_VARS_NAMES` and `CAPTURED_VARS_ARRAY` (instead of `CAPTURED_VARS_ARRAY` that was space-delimited)

--- lib/functions/logging/capture.sh
+++ lib/functions/logging/capture.sh
@@ -9,7 +9,7 @@
 
 function do_capturing_defs() {
 	# make sure to local with a value, otherwise they will appear in the list...
-	local pre_exec_vars="" post_exec_vars="" new_vars_list="" onevar="" all_vars_array=()
+	declare pre_exec_vars="" post_exec_vars="" new_vars_list="" onevar="" all_vars_array=()
 	pre_exec_vars="$(compgen -A variable | grep -E '[[:upper:]]+' | grep -v -e "^BASH_" | sort)"
 
 	# run parameters passed. if this fails, so will we, immediately, and not capture anything correctly.
@@ -20,13 +20,12 @@ function do_capturing_defs() {
 	new_vars_list="$(comm -13 <(echo "$pre_exec_vars") <(echo "${post_exec_vars}"))"
 
 	for onevar in ${new_vars_list}; do
-		# @TODO: rpardini: handle arrays and maps specially?
 		all_vars_array+=("$(declare -p "${onevar}")")
 	done
-	#IFS=$'\n'
-	export CAPTURED_VARS="${all_vars_array[*]}"
-	#display_alert "Vars defined during ${*@Q}:" "${CAPTURED_VARS}" "debug"
-	unset all_vars_array post_exec_vars new_vars_list pre_exec_vars onevar join_by
+
+	declare -g CAPTURED_VARS_NAMES="${new_vars_list}"
+	declare -ga CAPTURED_VARS_ARRAY=("${all_vars_array[@]}")
+	unset all_vars_array post_exec_vars new_vars_list pre_exec_vars onevar
 
 	return 0 # return success explicitly , preemptively preventing short-circuit problems.
 }
