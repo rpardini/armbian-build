191a496161f8bd2d12cc12edaf1f698e22a869b7
Ricardo Pardini <ricardo@pardini.net>
2023-03-28 01:11:34 +0200

    include rudimentary artifact dependency information for config-dump-json

--- lib/functions/cli/cli-configdump.sh
+++ lib/functions/cli/cli-configdump.sh
@@ -23,6 +23,38 @@ function cli_config_dump_json_run() {
 
 function config_board_and_remove_useless() {
 	skip_host_config=yes use_board=yes skip_kernel=no do_logging=no prep_conf_main_minimal_ni # avoid logging during configdump; it's useless; skip host config
+
+	# Determine which artifacts to build.
+	declare -a artifacts_to_build=()
+	determine_artifacts_to_build_for_image
+	display_alert "Artifacts to build:" "${artifacts_to_build[*]}" "warn"
+
+	# For each artifact, get the input variables from each.
+	declare -a all_wanted_artifact_names=() all_wanted_artifact_vars=()
+	declare one_artifact one_artifact_package
+	for one_artifact in "${artifacts_to_build[@]}"; do
+		declare -g artifact_input_vars
+
+		WHAT="${one_artifact}" dump_artifact_config
+
+		declare WHAT_UPPERCASE="${one_artifact^^}"
+		declare WHAT_UPPERCASE_REPLACED="${WHAT_UPPERCASE//[-.]/_}"
+		all_wanted_artifact_names+=("${one_artifact}")
+		all_wanted_artifact_vars+=("${WHAT_UPPERCASE_REPLACED}")
+
+		declare evalstr="declare -r -g WANT_ARTIFACT_${WHAT_UPPERCASE_REPLACED}_NAME=\"${one_artifact}:${WHAT_UPPERCASE_REPLACED}\""
+		display_alert "Eval string" "${evalstr}" "warn"
+		eval "${evalstr}"
+
+		declare evalstr="declare -r -g WANT_ARTIFACT_${WHAT_UPPERCASE_REPLACED}_INPUTS_ARRAY=\"${artifact_input_vars}\""
+		display_alert "Eval string" "${evalstr}" "warn"
+		eval "${evalstr}"
+
+	done
+
+	declare -r -g WANT_ARTIFACT_ALL_NAMES_ARRAY="${all_wanted_artifact_names[*]}"
+	declare -r -g WANT_ARTIFACT_ALL_ARRAY="${all_wanted_artifact_vars[*]}"
+
 	unset FINALDEST
 	unset DEB_STORAGE
 	unset ROOTPWD
