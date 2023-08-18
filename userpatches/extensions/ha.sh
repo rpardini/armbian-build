38ecf0f796f1b8c0172ec3fe250e0041506d39af
Ricardo Pardini <ricardo@pardini.net>
2023-08-18 20:08:47 +0200

    rpardini: exts: ha: preadd the supervised deb to image; ensure bookworm or bullseye

--- userpatches/extensions/ha.sh
+++ userpatches/extensions/ha.sh
@@ -1,16 +1,19 @@
 enable_extension "docker-ce"
 
-# Stuff: download and install amd64 https://github.com/home-assistant/os-agent/releases/download/1.5.1/os-agent_1.5.1_linux_x86_64.deb
-#                             arm64 https://github.com/home-assistant/os-agent/releases/download/1.5.1/os-agent_1.5.1_linux_aarch64.deb
-#                             armhf https://github.com/home-assistant/os-agent/releases/download/1.5.1/os-agent_1.5.1_linux_armv7.deb
-
-# @TODO: cgroupv1: systemd.unified_cgroup_hierarchy=false to kernel cmdline
-
 function extension_prepare_config__home_assistant() {
-	display_alert "Target image will be a Home Assistant Supervised machine" "${EXTENSION}" "info"
+	display_alert "Target image will be a Home Assistant Supervised deploy" "${EXTENSION}" "info"
 	declare -g EXTRA_BSP_NAME="${EXTRA_BSP_NAME}-ha" # Unique bsp name for this extension
 
 	declare -g KEEP_ORIGINAL_OS_RELEASE="yes" # Keep original os-release file when installing bsp and preparing image
+
+	case "${RELEASE}" in
+		bullseye | bookworm)
+			display_alert "Setting up Home Assistant Supervised on Debian ${RELEASE}" "${EXTENSION}" "info"
+			;;
+		*)
+			exit_with_error "Home Assistant Supervised is not supported on ${DISTRIBUTION} ${RELEASE}"
+			;;
+	esac
 
 	# HA-Supervised wants cgroupsv1, no idea why, but such is life.
 	declare -g GRUB_CMDLINE_LINUX_DEFAULT="${GRUB_CMDLINE_LINUX_DEFAULT} systemd.unified_cgroup_hierarchy=false" # GRUB version
@@ -27,12 +30,21 @@ function extension_prepare_config__home_assistant() {
 	[[ "${ARCH}" == "arm64" ]] && declare -g HA_OS_AGENT_ARCH="aarch64"
 	[[ "${ARCH}" == "amd64" ]] && declare -g HA_OS_AGENT_ARCH="x86_64"
 
+	declare -g HA_OS_AGENT_CACHE_DIR="${SRC}/cache/home_assistant_debs"
+
+	# os-agent   deb: amd64 https://github.com/home-assistant/os-agent/releases/download/1.5.1/os-agent_1.5.1_linux_x86_64.deb
+	# os-agent   deb: arm64 https://github.com/home-assistant/os-agent/releases/download/1.5.1/os-agent_1.5.1_linux_aarch64.deb
+	# os-agent   deb: armhf https://github.com/home-assistant/os-agent/releases/download/1.5.1/os-agent_1.5.1_linux_armv7.deb
 	declare -g HA_OS_AGENT_VERSION="1.5.1"
 	declare -g HA_OS_AGENT_FILENAME="os-agent_${HA_OS_AGENT_VERSION}_linux_${HA_OS_AGENT_ARCH}.deb"
-
 	declare -g HA_OS_AGENT_URL="https://github.com/home-assistant/os-agent/releases/download/${HA_OS_AGENT_VERSION}/${HA_OS_AGENT_FILENAME}"
-	declare -g HA_OS_AGENT_CACHE_DIR="${SRC}/cache/home_assistant_debs"
 	declare -g HA_OS_AGENT_CACHE_FILE="${HA_OS_AGENT_CACHE_DIR}/${HA_OS_AGENT_FILENAME}"
+
+	# supervised deb: all
+	declare -g HA_SUPERVISED_VERSION="1.5.0"
+	declare -g HA_SUPERVISED_FILENAME="homeassistant-supervised_${HA_SUPERVISED_VERSION}.deb"
+	declare -g HA_SUPERVISED_URL="https://github.com/home-assistant/supervised-installer/releases/download/${HA_SUPERVISED_VERSION}/homeassistant-supervised.deb"
+	declare -g HA_SUPERVISED_CACHE_FILE="${HA_OS_AGENT_CACHE_DIR}/${HA_SUPERVISED_FILENAME}"
 
 	display_alert "Adding HA packages" "${EXTENSION}" "info"
 	add_packages_to_image systemd-journal-remote apparmor
@@ -44,10 +56,16 @@ function extension_prepare_config__home_assistant() {
 	display_alert "Fetching Home Assistant debs" "${EXTENSION}" "info"
 	mkdir -p "${HA_OS_AGENT_CACHE_DIR}"
 	if [[ -f "${HA_OS_AGENT_CACHE_FILE}" ]]; then
-		display_alert "Using cached Home Assistant deb" "${HA_OS_AGENT_CACHE_FILE}" "info"
+		display_alert "Using cached Home Assistant os-agent deb" "${HA_OS_AGENT_CACHE_FILE}" "info"
 	else
-		display_alert "Fetching Home Assistant deb" "${HA_OS_AGENT_URL}" "info"
+		display_alert "Fetching Home Assistant os-agent deb" "${HA_OS_AGENT_URL}" "info"
 		run_host_command_logged wget --progress=dot:giga --output-document="${HA_OS_AGENT_CACHE_FILE}" "${HA_OS_AGENT_URL}"
+	fi
+	if [[ -f "${HA_SUPERVISED_CACHE_FILE}" ]]; then
+		display_alert "Using cached Home Assistant supervised deb" "${HA_SUPERVISED_CACHE_FILE}" "info"
+	else
+		display_alert "Fetching Home Assistant supervised deb" "${HA_SUPERVISED_URL}" "info"
+		run_host_command_logged wget --progress=dot:giga --output-document="${HA_SUPERVISED_CACHE_FILE}" "${HA_SUPERVISED_URL}"
 	fi
 
 	# @TODO: at first run, install the supervised .deb.
@@ -57,6 +75,7 @@ function pre_install_kernel_debs__add_home_assistant_debs_to_image() {
 	display_alert "Adding Home Assistant debs to image" "${EXTENSION}" "info"
 	run_host_command_logged mkdir -p "${SDCARD}"/opt/hainstall
 	run_host_command_logged cp -pv "${HA_OS_AGENT_CACHE_FILE}" "${SDCARD}/opt/hainstall/${HA_OS_AGENT_FILENAME}"
+	run_host_command_logged cp -pv "${HA_SUPERVISED_CACHE_FILE}" "${SDCARD}/opt/hainstall/${HA_SUPERVISED_FILENAME}"
 	chroot_sdcard ls -la /opt/hainstall
 	chroot_sdcard_apt_get_install "/opt/hainstall/${HA_OS_AGENT_FILENAME}"
 }
