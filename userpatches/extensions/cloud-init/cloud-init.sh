3b8bda0e19e827cfdd82ed7f5e4185b1542c5159
Ricardo Pardini <ricardo@pardini.net>
2023-10-01 09:27:44 +0200

    rpardini: exts: cloud-init: trixie lost `thin-provisioning-tools`

--- userpatches/extensions/cloud-init/cloud-init.sh
+++ userpatches/extensions/cloud-init/cloud-init.sh
@@ -73,7 +73,11 @@ function extension_prepare_config__990_late_finish_cloud_init_config() {
 	fi
 
 	ci_packages_install+=("cloud-init" "cloud-initramfs-growroot" "busybox" "eatmydata" "curl" "tree") # 'busybox' helps with growroot working on bookworm
-	ci_packages_install+=("lvm2" "thin-provisioning-tools" "systemd-timesyncd" "wpasupplicant")
+	ci_packages_install+=("lvm2" "systemd-timesyncd" "wpasupplicant")
+
+	if [[ "${RELEASE}" != "trixie" ]]; then # Hack, trixie recently (2023-10-01) lost this package for some reason
+		ci_packages_install+=("thin-provisioning-tools")
+	fi
 
 	# This means "add to rootfs cache list", not "for this board".
 	add_packages_to_rootfs "${ci_packages_install[@]}"
