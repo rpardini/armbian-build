060bd5901d0cdd0cc0a1c08fccc62517371fe27a
Ricardo Pardini <ricardo@pardini.net>
2025-05-25 09:30:38 +0200

    rpardini: exts: cloud-metadata: set SERIALCON ttyAMA0/ttyS0 unconditionally

--- userpatches/extensions/cloud-metadata.sh
+++ userpatches/extensions/cloud-metadata.sh
@@ -6,12 +6,12 @@ function user_config__metadata_cloud_config_common() {
 
 function user_config__metadata_cloud_config_arm64() {
 	[[ "${ARCH}" != "arm64" ]] && return 0
-	display_alert "Configuring cloud-init for arm64 metadata" "cloud-metadata: arm64" "info"
-	declare -g SERIALCON="${SERIALCON:-"ttyAMA0"}" # For ORACLE cloud vm; others with serial console
+	declare -g SERIALCON="ttyAMA0" # Default serial console for arm64
+	display_alert "Configuring cloud-init for arm64 metadata" "cloud-metadata: arm64; SERIALCON=${SERIALCON}" "warn"
 }
 
 function user_config__metadata_cloud_config_amd64_x86() {
 	[[ "${ARCH}" != "amd64" ]] && return 0
-	display_alert "Configuring cloud-init for amd64 metadata" "cloud-metadata: amd64" "info"
-	declare -g SERIALCON="${SERIALCON:-"ttyS0"}" # For Oracle VMs on AMD micro stuff; AWS with serial console, etc.
+	declare -g SERIALCON="ttyS0" # Default serial console for amd64
+	display_alert "Configuring cloud-init for amd64 metadata" "cloud-metadata: amd64; SERIALCON=${SERIALCON}" "warn"
 }
