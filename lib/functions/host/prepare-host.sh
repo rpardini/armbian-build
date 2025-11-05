74b36152975434ae4d8777de7ff9aff693e21f5e
Ricardo Pardini <ricardo@pardini.net>
2025-11-05 14:51:16 +0100

    KILL python3-setuptools

--- lib/functions/host/prepare-host.sh
+++ lib/functions/host/prepare-host.sh
@@ -215,8 +215,8 @@ function adaptative_prepare_host_dependencies() {
 
 	# Some versions of U-Boot do not require/import 'python3-setuptools' properly, so add them explicitly.
 	if [[ 'tag:v2022.04' == "${BOOTBRANCH:-}" || 'tag:v2022.07' == "${BOOTBRANCH:-}" ]]; then
-		display_alert "Adding package to 'host_dependencies'" "python3-setuptools" "info"
-		host_dependencies+=("python3-setuptools")
+		display_alert "NOT Adding package to 'host_dependencies'" "python3-setuptools due to BOOTBRANCH=${BOOTBRANCH}" "warn"
+		#host_dependencies+=("python3-setuptools")
 	fi
 
 	# qemu binfmt + chrooted user emulation.
