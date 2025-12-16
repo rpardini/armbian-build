1c360d3f2b732f72dfae4dfeabcedac7d02210c3
Ricardo Pardini <ricardo@pardini.net>
2025-12-16 23:11:11 +0100

    rpardini: exts: cloud-metadata-initrd pseudoext
    - qcow2 sucks for TB provisioning
    - need to adapt cloud-container-disk later

---
+++ userpatches/extensions/cloud-metadata-initrd.sh
@@ -0,0 +1,6 @@
+enable_extension "cloud-metadata"
+
+function user_config__metadata_qcow_initrd() {
+	display_alert "Configuring cloud-init" "cloud-metadata-qcow2-initrd: initrd & qcow2" "info"
+	declare -g UEFI_EXPORT_KERNEL_INITRD="yes" # Export the initrd and kernel for meta, just like DKB
+}
