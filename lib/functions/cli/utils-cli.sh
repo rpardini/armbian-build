6765d1dc6febe2883540c6a064cc3da9b09c3fc0
Ricardo Pardini <ricardo@pardini.net>
2026-06-28 19:55:24 +0200

    HACK: PREFER_DOCKER=yes forces docker even if already running as root

--- lib/functions/cli/utils-cli.sh
+++ lib/functions/cli/utils-cli.sh
@@ -223,7 +223,7 @@ function produce_relaunch_parameters() {
 
 function cli_standard_relaunch_docker_or_sudo() {
 	display_alert "Gonna relaunch" "EUID: ${EUID} -- PREFER_DOCKER:${PREFER_DOCKER}" "debug"
-	if [[ "${EUID}" == "0" ]]; then # we're already root. Either running as real root, or already sudo'ed.
+	if [[ "${EUID}" == "0" && "${PREFER_DOCKER:-no}" == "no" ]]; then # we're already root. Either running as real root, or already sudo'ed.
 		if [[ "${ARMBIAN_RELAUNCHED}" != "yes" && "${ALLOW_ROOT}" != "yes" ]]; then
 			display_alert "PROBLEM: don't run ./compile.sh as root or with sudo" "PROBLEM: don't run ./compile.sh as root or with sudo" "err"
 			if [[ -t 0 ]]; then # so... non-interactive builds *can* run as root. It's not supported, you'll get an error, but we'll proceed.
